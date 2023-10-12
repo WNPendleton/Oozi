8f56f3ba7292860aab5bac4f78eb0bf42ed2ede1
WNPendleton <WNPendleton@gmail.com>
2023-10-12 12:41:21 -0500

    Goo arm basic

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,29 +1,20 @@
 extends CharacterBody3D
 
-@export var look_speed = 0.01
-@export var max_rot_y = PI/2
-
-var rot_x = 0
-var rot_y = 0
+@export var arm_range : Vector2
+@export var arm_base : Vector2
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-func _input(event):
-	if event is InputEventMouseMotion:
-		rot_x += event.relative.x * look_speed
-		rot_y += event.relative.y * look_speed
-		if rot_y > max_rot_y:
-			rot_y = max_rot_y
-		if rot_y < -max_rot_y:
-			rot_y = -max_rot_y
-		transform.basis = Basis()
-		$Camera.transform.basis = Basis()
-		rotate_object_local(Vector3(0,1,0), -rot_x)
-		$Camera.rotate_object_local(Vector3(1,0,0), -rot_y)
+	
+	var mouse_pos = get_viewport().get_mouse_position()
+	var screen_size = get_viewport().size
+	var mouse_offset = Vector2(float(mouse_pos.x)/screen_size.x-0.5, float(mouse_pos.y)/screen_size.y-0.5)
+	$ArmAnchor.transform.basis = Basis()
+	$ArmAnchor.rotate_object_local(Vector3(0,1,0), arm_base.x - (arm_range.x * mouse_offset.x))
+	$ArmAnchor.rotate_object_local(Vector3(1,0,0), arm_base.y - (arm_range.y * mouse_offset.y))
--- scripts/UI.gd
+++ scripts/UI.gd
@@ -1,10 +1,13 @@
 extends Control
 
+var previous_mouse_mode
+
 func _process(_delta):
 	if Input.is_action_just_pressed("pause"):
 		var tree = get_tree()
 		tree.paused = !tree.paused
 		if tree.paused:
+			previous_mouse_mode = Input.mouse_mode
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		else:
-			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			Input.mouse_mode = previous_mouse_mode
